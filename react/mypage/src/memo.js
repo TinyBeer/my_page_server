a3590b7e619ad87787c650529453550356e7f7fa
tinybeer <543595604@qq.com>
2024-05-22 01:18:50 +0800

    memo && video

--- react/mypage/src/memo.js
+++ react/mypage/src/memo.js
@@ -1,19 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
   EditOutlined,
   BgColorsOutlined,
   DeleteOutlined,
+  ArrowLeftOutlined,
 } from '@ant-design/icons';
 import { Input, Layout, Card, Row, Col } from 'antd';
 import { refreshToken } from './store/module/tokenStore';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchMemoList } from './store/module/memoStore';
-
+import {
+  createMemo,
+  fetchMemoList,
+  removeMemoById,
+} from './store/module/memoStore';
+import './css/memo.css';
 const { Meta } = Card;
 const { Header, Content } = Layout;
 
 export default function App() {
+  const [refresh, setRefresh] = useState(false);
+  const [inputValue, setInputValue] = useState('');
   const [loginStatus, setLoginStatus] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -30,10 +37,12 @@ export default function App() {
     };
 
     run();
-  }, [dispatch]);
+  }, [dispatch, refresh]);
 
   const memoList = useSelector((state) => state.memo.memoList);
-  const onChange = (e) => {};
+  const onChange = (e) => {
+    setInputValue(e.target.value);
+  };
   return (
     <Layout>
       <Header
@@ -44,64 +53,79 @@ export default function App() {
         }}
       >
         <Input
+          value={inputValue}
           style={{ minWidth: '240px', width: '50%' }}
           showCount
           maxLength={20}
           onChange={onChange}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              dispatch(
+                createMemo(e.target.value, () => {
+                  setInputValue('');
+                  setRefresh((pre) => !pre);
+                })
+              );
+            }
+          }}
           placeholder='请输入要做的事？'
         />
       </Header>
       <Content style={{ padding: '0 48px', margin: '24px 16px' }}>
         <Row gutter={24}>
-          {memoList.map((item, index) => {
-            const key = `${item.id}`;
-            return (
-              <Col
-                style={{ margin: '4px 8px 20px 8px' }}
-                key={key}
-                xs={{
-                  flex: '100%',
-                }}
-                sm={{
-                  flex: '50%',
-                }}
-                md={{
-                  flex: '40%',
-                }}
-                lg={{
-                  flex: '20%',
-                }}
-                xl={{
-                  flex: '10%',
-                }}
-              >
-                <Card
-                  style={{
-                    width: 240,
-                    padding: '8 0',
+          {Array.isArray(memoList) &&
+            memoList.slice(0, 8).map((item, index) => {
+              const key = `${item.id}`;
+              return (
+                <Col
+                  style={{ margin: '4px 8px 20px 8px' }}
+                  key={key}
+                  xs={{
+                    flex: '100%',
                   }}
-                  hoverable='true'
-                  size='small'
-                  cover={
-                    <img
-                      alt='example'
-                      src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'
-                    />
-                  }
-                  actions={[
-                    <DeleteOutlined key='delete' />,
-                    <EditOutlined key='edit' />,
-                    <BgColorsOutlined
-                      key='pic'
-                      onClick={() => navigate('/nav')}
-                    />,
-                  ]}
+                  sm={{
+                    flex: '50%',
+                  }}
+                  md={{
+                    flex: '40%',
+                  }}
+                  lg={{
+                    flex: '20%',
+                  }}
+                  xl={{
+                    flex: '10%',
+                  }}
                 >
-                  <Meta title={item.content} />
-                </Card>
-              </Col>
-            );
-          })}
+                  <Card
+                    style={{
+                      width: 240,
+                      padding: '8 0',
+                    }}
+                    hoverable='true'
+                    size='small'
+                    cover={
+                      <img
+                        alt='example'
+                        src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'
+                      />
+                    }
+                    actions={[
+                      <DeleteOutlined
+                        key='delete'
+                        onClick={() => dispatch(removeMemoById(item.id))}
+                      />,
+                      <EditOutlined key='edit' />,
+                      <BgColorsOutlined
+                        key='pic'
+                        onClick={() => navigate('/nav')}
+                      />,
+                    ]}
+                  >
+                    <Meta title={item.content} />
+                  </Card>
+                </Col>
+              );
+            })}
         </Row>
       </Content>
     </Layout>
--- react/mypage/src/store/module/memoStore.js
+++ react/mypage/src/store/module/memoStore.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
+import _ from 'lodash';
 
 const memoStore = createSlice({
   name: 'memo',
@@ -10,23 +11,91 @@ const memoStore = createSlice({
     setMemoList(state, action) {
       state.memoList = action.payload;
     },
+    removeFromList(state, action) {
+      state.memoList = _.filter(
+        state.memoList,
+        (item) => item.id !== action.payload
+      );
+    },
   },
 });
 
-const { setMemoList } = memoStore.actions;
+const { setMemoList, removeFromList } = memoStore.actions;
+
+const createInstance = () => {
+  const access_token = localStorage.getItem('access_token');
+  return axios.create({
+    baseURL: 'http://127.0.0.1:9999',
+    timeout: 1000,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: access_token,
+    },
+  });
+};
+
+function createMemo(content, success, failed) {
+  return (dispatch) => {
+    createInstance()
+      .post('/memo/create', JSON.stringify({ content }))
+      .then((res) => res.data)
+      .then((data) => {
+        if (data.status === 'success') {
+          if (success) {
+            success();
+          }
+        } else {
+          throw new Error(data.message);
+        }
+      })
+      .catch((error) => {
+        console.error('add memo error:', error);
+        if (failed) {
+          failed();
+        }
+      });
+  };
+}
+
+function removeMemoById(id, success, failed) {
+  return (dispatch) => {
+    // axios默认不支持delete带body
+    const access_token = localStorage.getItem('access_token');
+    const data = JSON.stringify({ id });
+    axios
+      .create({
+        baseURL: 'http://127.0.0.1:9999',
+        timeout: 1000,
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: access_token,
+        },
+      })
+      .delete('/memo/delete', { data })
+      .then((res) => res.data)
+      .then((data) => {
+        if (data.status === 'success') {
+          dispatch(removeFromList(id));
+          if (success) {
+            success();
+          }
+        } else {
+          throw new Error(data.message);
+        }
+      })
+      .catch((error) => {
+        console.error('delete memo error:', error);
+        if (failed) {
+          failed();
+        }
+      });
+  };
+}
 
 function fetchMemoList(success, failed) {
   return (dispatch) => {
-    const access_token = localStorage.getItem('access_token');
-    const instance = axios.create({
-      baseURL: 'http://127.0.0.1:9999',
-      timeout: 1000,
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: access_token,
-      },
-    });
-    instance
+    createInstance()
       .get('/memo/list')
       .then((res) => res.data)
       .then((data) => {
@@ -50,5 +119,5 @@ function fetchMemoList(success, failed) {
 
 const reducer = memoStore.reducer;
 
-export { fetchMemoList };
+export { fetchMemoList, removeMemoById, createMemo };
 export default reducer;
